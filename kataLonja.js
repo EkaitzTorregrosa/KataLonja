2d9cee91ea0d46317cd02a9954193f5490a3f485
Ekaitz Torregrosa <ekaitztorregrosa@MBP-de-Pablo.home>
2021-04-07 08:40:05 +0200

    first commit

--- kataLonja.js
+++ kataLonja.js
@@ -1,5 +1,23 @@
 "use strict";
+exports.__esModule = true;
+exports.datosMercancia = void 0;
 var ganancias = {};
+function datosMercancia(kgVieiras, kgPulpo, kgCentollo) {
+    var precioEstimadoMadrid = calcularPrecio("vieira", kgVieiras, "madrid") + calcularPrecio("pulpo", kgPulpo, "madrid") + calcularPrecio("centollo", kgCentollo, "madrid");
+    var precioEstimadoBarcelona = calcularPrecio("vieira", kgVieiras, "barcelona") + calcularPrecio("pulpo", kgPulpo, "barcelona") + calcularPrecio("centollo", kgCentollo, "barcelona");
+    var precioEstimadoLisboa = calcularPrecio("vieira", kgVieiras, "lisboa") + calcularPrecio("pulpo", kgPulpo, "lisboa") + calcularPrecio("centollo", kgCentollo, "lisboa");
+    calcularGanancias("madrid", precioEstimadoMadrid);
+    calcularGanancias("barcelona", precioEstimadoBarcelona);
+    calcularGanancias("lisboa", precioEstimadoLisboa);
+    var items = Object.keys(ganancias).map(function (key) {
+        return [key[0].toUpperCase() + key.slice(1), ganancias[key]];
+    });
+    items.sort(function (first, second) {
+        return second[1] - first[1];
+    });
+    return "El sitio donde obtendr\u00E1 mayor beneficio es " + items[0][0] + ", con un beneficio de " + items[0][1] + " \u20AC";
+}
+exports.datosMercancia = datosMercancia;
 function calcularPrecio(producto, kilos, ciudad) {
     var precioProductosPorCiudad = {
         "vieira": { "madrid": 500, "barcelona": 450, "lisboa": 600 },
@@ -20,19 +38,3 @@ function calcularGanancias(ciudad, precioEstimado) {
     var precioFinal = (precioEstimado - (5 + 2 * distanciaACiudad)) * ((100 - (distanciaACiudad / 100)) / 100);
     ganancias[ciudad] = precioFinal.toFixed(2);
 }
-var precioEstimadoMadrid = calcularPrecio("vieira", 50, "madrid") + calcularPrecio("pulpo", 100, "madrid") + calcularPrecio("centollo", 50, "madrid");
-//console.log(`Precio estimado en Madrid : ${precioEstimadoMadrid} €`);
-var precioEstimadoBarcelona = calcularPrecio("vieira", 50, "barcelona") + calcularPrecio("pulpo", 100, "barcelona") + calcularPrecio("centollo", 50, "barcelona");
-//console.log(`Precio estimado en Barcelona : ${precioEstimadoBarcelona} €`);
-var precioEstimadoLisboa = calcularPrecio("vieira", 50, "lisboa") + calcularPrecio("pulpo", 100, "lisboa") + calcularPrecio("centollo", 50, "lisboa");
-//console.log(`Precio estimado en Lisboa : ${precioEstimadoLisboa} €`);
-calcularGanancias("madrid", precioEstimadoMadrid);
-calcularGanancias("barcelona", precioEstimadoBarcelona);
-calcularGanancias("lisboa", precioEstimadoLisboa);
-var items = Object.keys(ganancias).map(function (key) {
-    return [key[0].toUpperCase() + key.slice(1), ganancias[key]];
-});
-items.sort(function (first, second) {
-    return second[1] - first[1];
-});
-console.log("El sitio donde obtendr\u00E1 mayor beneficio es " + items[0][0] + ", con un beneficio de " + items[0][1] + " \u20AC");
